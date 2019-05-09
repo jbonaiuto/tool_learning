a8452ac939b6b447fed60e1045da1d1d34679406
jbonaiuto <jbonaiuto@gmail.com>
2019-05-09 20:00:47 +0200

    find change points based on mean signal from whole array

--- src/matlab/lfp/clean_jumps.m
+++ src/matlab/lfp/clean_jumps.m
@@ -1,38 +1,54 @@
-function clean_chan_data=clean_jumps(chan_data)
+function clean_array_data=clean_jumps(array_data)
 
-chan_diff=diff(chan_data);
-[jumps,residual] = findchangepts(chan_data,'Statistic','mean','MinThreshold',50000,'MinDistance',100);
+%[jumps,residual] = findchangepts(chan_data,'Statistic','linear','MinThreshold',50000,'MinDistance',50);
+[jumps,residual] = findchangepts(mean(array_data),'Statistic','linear','MinThreshold',7000);
 
-clean_chan_data=chan_data;
+clean_array_data=array_data;
 if length(jumps)>0
-    % Mean of signal 100 time steps before first jump
-    last_seg_mean = mean(clean_chan_data(max(1,jumps(1)-100):jumps(1)));
-    for i = 1 : length(jumps)-1
-        % Mean of signal 100 time steps after current jump
-        curr_seg_mean=mean(clean_chan_data(jumps(i):min(jumps(i)+100,length(clean_chan_data))));
-        clean_chan_data(jumps(i):jumps(i+1)) = clean_chan_data(jumps(i):jumps(i+1)) - (curr_seg_mean-last_seg_mean);
-        last_seg_mean=mean(clean_chan_data(max(1,jumps(i+1)-100):jumps(i+1)));
+    for ch_idx=1:size(clean_array_data,1)
+        chan_data=clean_array_data(ch_idx,:);
+        
+        % Mean of signal 100 time steps before first jump
+        last_seg_mean = mean(chan_data(max(1,jumps(1)-100):jumps(1)));
+        for i = 1 : length(jumps)-1
+            % Mean of signal 100 time steps after current jump
+            curr_seg_mean=mean(chan_data(jumps(i):min(jumps(i)+100,length(chan_data))));
+            chan_data(jumps(i):jumps(i+1)) = chan_data(jumps(i):jumps(i+1)) - (curr_seg_mean-last_seg_mean);
+            last_seg_mean=mean(chan_data(max(1,jumps(i+1)-100):jumps(i+1)));
+        end
+        curr_seg_mean=mean(chan_data(jumps(end):min(jumps(end)+100,length(chan_data))));
+        chan_data(jumps(end):end) = chan_data(jumps(end):end) - (curr_seg_mean-last_seg_mean);
+
+        % Get rid of jump removal artifact
+        for i=1:length(jumps)
+            chan_data(jumps(i)-1:min(length(chan_data),jumps(i)+1))=NaN;
+        end
+        nanx = isnan(chan_data);
+        t    = 1:numel(chan_data);
+        chan_data(nanx) = interp1(t(~nanx), chan_data(~nanx), t(nanx));
+        clean_array_data(ch_idx,:)=chan_data;
     end
-    curr_seg_mean=mean(clean_chan_data(jumps(end):min(jumps(end)+100,length(clean_chan_data))));
-    clean_chan_data(jumps(end):end) = clean_chan_data(jumps(end):end) - (curr_seg_mean-last_seg_mean);
 end
 
 
 % If there are any jumps
-% if length(jumps)>0
-%     figure();
-%     subplot(2,1,1);
-%     plot(chan_data);
-%     hold on;
-%     for j=1:length(jumps)
-%         plot([jumps(j) jumps(j)],ylim(),'r');
-%     end
-%     subplot(2,1,2);
-%     plot(clean_chan_data);
-%     disp('');
-% else
-%     figure();
-%     subplot(2,1,1);
-%     plot(chan_data);
-% end          
-% disp('');
+figure();
+subplot(3,1,1);
+plot(mean(array_data));
+hold on;
+for j=1:length(jumps)
+    plot([jumps(j) jumps(j)],ylim(),'r');
+end
+subplot(3,1,2);
+plot(array_data');
+hold on;
+for j=1:length(jumps)
+    plot([jumps(j) jumps(j)],ylim(),'r');
+end
+subplot(3,1,3);
+plot(clean_array_data');
+hold on;
+for j=1:length(jumps)
+    plot([jumps(j) jumps(j)],ylim(),'r');
+end
+disp('');
