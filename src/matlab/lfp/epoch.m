612ed59699e0bbe7be455a90f28231f585dd3147
jbonaiuto <jbonaiuto@gmail.com>
2019-08-16 10:06:20 +0200

    specify conditions and whether or not to exclude bad trials

--- src/matlab/lfp/epoch.m
+++ src/matlab/lfp/epoch.m
@@ -1,4 +1,4 @@
-function epoch_data=epoch(exp_info, data, evt, limits, varargin)
+function epoch_data=epoch(exp_info, data, evt, limits, conditions, varargin)
 
 % Parse optional arguments
 defaults=struct('exclude_bad_trials',true);
@@ -24,11 +24,17 @@ epoch_data.trial={};
 epoch_data.trialinfo=[];
 
 trials_to_use=[1:length(data.trial)];
+if ~strcmp(conditions,'all')
+    trials_to_use=[];
+    for i=1:length(conditions)
+        cond_idx=find(strcmp(exp_info.conditions,conditions{i}));
+        trials_to_use=union(trials_to_use, find(data.trialinfo(:,1)==cond_idx));
+    end
+end
 if params.exclude_bad_trials
-    trials_to_user=find(data.trialinfo(:,2)==1);
+    trials_to_use=intersect(trials_to_use,find(data.trialinfo(:,2)==1));
 end
 
-error=false;
 for i=1:length(trials_to_use)
     t=trials_to_use(i);
     trial_data=data.trial{t};
@@ -36,24 +42,12 @@ for i=1:length(trials_to_use)
         zeroed_times=data.time{t}-data.trialinfo(t,evt_idx+2);
         if zeroed_times(1)>limits(1) || zeroed_times(end)<limits(2)
             disp(sprintf('Limits out of bounds for trial %d, %.2f-%.2f', t, zeroed_times(1), zeroed_times(end)));
-            error=true;
-        end
-    end
-end
-
-if ~error
-    for i=1:length(trials_to_use)
-        t=trials_to_use(i);
-        trial_data=data.trial{t};
-        if ~isnan(data.trialinfo(t,evt_idx+2))
-            zeroed_times=data.time{t}-data.trialinfo(t,evt_idx+2);
-            if zeroed_times(1)<=limits(1) && zeroed_times(end)>=limits(2)
-                idx=intersect(find(zeroed_times>=limits(1)),find(zeroed_times<=limits(2)));
-                epoch_data.time{end+1}=zeroed_times(idx);
-                epoch_data.trial{end+1}=trial_data(:,idx);
-                epoch_data.trialinfo(end+1,:)=data.trialinfo(t,:);
-                epoch_data.trialinfo(end,3:end)=epoch_data.trialinfo(end,3:end)-data.trialinfo(t,evt_idx+2);
-            end
+        else
+            idx=intersect(find(zeroed_times>=limits(1)),find(zeroed_times<=limits(2)));
+            epoch_data.time{end+1}=zeroed_times(idx);
+            epoch_data.trial{end+1}=trial_data(:,idx);
+            epoch_data.trialinfo(end+1,:)=data.trialinfo(t,:);
+            epoch_data.trialinfo(end,3:end)=epoch_data.trialinfo(end,3:end)-data.trialinfo(t,evt_idx+2);
         end
     end
 end
