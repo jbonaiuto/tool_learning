6b11e4820fce797ff48095f9a346c7b175cc7be1
jbonaiuto <jbonaiuto@gmail.com>
2019-10-08 14:36:20 +0200

    initial import

---
+++ src/python/multiunit/run_peeler.py
@@ -0,0 +1,98 @@
+import matplotlib
+matplotlib.use('TkAgg')
+
+import os
+import sys
+import time
+import pyqtgraph as pg
+import numpy as np
+import gc
+import pandas as pd
+from tridesclous import DataIO, Peeler, PeelerWindow
+
+# This is for selecting a GPU
+# for e in tdc.get_cl_device_list():
+#     print(e)
+# tdc.set_default_cl_device(platform_index=0, device_index=0)
+
+from config import read_config
+
+cfg = read_config()
+
+def run_peeler(dirname, chan_grp):
+    dataio = DataIO(dirname=dirname, ch_grp=chan_grp)
+    initial_catalogue = dataio.load_catalogue(chan_grp=chan_grp)
+
+    peeler = Peeler(dataio)
+    peeler.change_params(catalogue=initial_catalogue,
+                         chunksize=32768,
+                         use_sparse_template=False,
+                         sparse_threshold_mad=1.5,
+                         use_opencl_with_sparse=False)
+
+    t1 = time.perf_counter()
+    peeler.run()
+    t2 = time.perf_counter()
+    print('peeler.run', t2 - t1)
+
+
+def open_PeelerWindow(dirname, chan_grp):
+    dataio = DataIO(dirname=dirname)
+    initial_catalogue = dataio.load_catalogue(chan_grp=chan_grp)
+
+    app = pg.mkQApp()
+    win = PeelerWindow(dataio=dataio, catalogue=initial_catalogue)
+    win.show()
+    app.exec_()
+
+
+def export_spikes(dirname, array_idx, chan_grp):
+    print('Exporting ch %d' % chan_grp)
+    data = {'array': [], 'electrode': [], 'segment': [], 'time': []}
+    array=cfg['arrays'][array_idx]
+
+
+    dataio = DataIO(dirname=dirname, ch_grp=chan_grp)
+    catalogue = dataio.load_catalogue(chan_grp=chan_grp)
+    dataio._open_processed_data(ch_grp=chan_grp)
+
+    clusters = catalogue['clusters']
+
+    for seg_num in range(dataio.nb_segment):
+        spikes = dataio.get_spikes(seg_num=seg_num, chan_grp=chan_grp)
+
+        spike_labels = spikes['cluster_label'].copy()
+        for l in clusters:
+            mask = spike_labels == l['cluster_label']
+            spike_labels[mask] = l['cell_label']
+        spike_indexes = spikes['index']
+
+        for (index,label) in zip(spike_indexes, spike_labels):
+            if label>=0:
+                data['array'].append(array)
+                data['electrode'].append(chan_grp)
+                data['segment'].append(seg_num)
+                data['time'].append(index)
+        dataio.flush_processed_signals(seg_num=seg_num, chan_grp=chan_grp)
+    df=pd.DataFrame(data, columns=['array','electrode','segment','time'])
+    df.to_csv(os.path.join(dirname,'%s_%d_spikes.csv' % (array, chan_grp)), index=False)
+
+
+if __name__ == '__main__':
+    subject=sys.argv[1]
+    recording_date=sys.argv[2]
+    display_peeler = sys.argv[3] in ['true', 'True','1']
+    export = sys.argv[4] in ['true', 'True', '1']
+
+    for array_idx in range(len(cfg['arrays'])):
+        output_dir = os.path.join(cfg['multi_unit_spike_sorting_dir'], subject, recording_date, 'multiunit', 'array_%d' % array_idx)
+        if os.path.exists(output_dir):
+            for ch_grp in range(cfg['n_channels_per_array']):
+                run_peeler(output_dir, chan_grp=ch_grp)
+                if display_peeler:
+                    open_PeelerWindow(output_dir, ch_grp)
+                if export:
+                    export_spikes(output_dir, array_idx, ch_grp)
+                gc.collect()
+
+
---
+++ src/python/process_multiunit.py
@@ -0,0 +1,162 @@
+import json
+import os
+from glob import glob
+from datetime import datetime, timedelta
+import pandas as pd
+import numpy as np
+import scipy.io
+import sys
+
+from config import read_config
+cfg = read_config()
+
+def run_process_multiunit(subj_name, date):
+    out_dir = os.path.join(cfg['preprocessed_data_dir'], subj_name, date, 'multiunit')
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+    spike_data_dir = os.path.join(cfg['multi_unit_spike_sorting_dir'], subj_name, date, 'multiunit')
+
+    rec_data_dir = os.path.join(cfg['preprocessed_data_dir'], subj_name, date,'rhd2000')
+    rec_fnames = glob(os.path.join(rec_data_dir, '*.json'))
+    rec_fdates = []
+    for rec_fname in rec_fnames:
+        fparts = os.path.splitext(rec_fname)[0].split('_')
+        try:
+            filedate = datetime.strptime('%s.%s' % (fparts[-2], fparts[-1]), '%d%m%y.%H%M%S')
+            rec_fdates.append(filedate)
+        except:
+            pass
+    rec_fnames = [x[1] for x in sorted(zip(rec_fdates, rec_fnames))]
+
+    seg_trial_start_idx=[]
+    seg_trial_end_idx=[]
+    seg_trial_start_evt_idx=[]
+    seg_times=[]
+    srate = cfg['intan_srate']
+
+    for rec_idx, rec_fname in enumerate(rec_fnames):
+        rec_data = json.load(open(rec_fname))
+        rec_signal = np.array(rec_data['rec_signal'])
+
+        time = np.linspace(1 / srate, rec_signal.size / srate, rec_signal.size)
+        seg_times.append(time)
+
+        # Find trial start and end points
+        trial_start = np.where(np.diff(rec_signal) == 1)[0]
+        trial_end = np.where(np.diff(rec_signal) == -1)[0]
+
+        trial_start_idx = []
+        trial_end_idx = []
+        trial_start_evt_idx = []
+
+        # If there is at least one start and stop time
+        if len(trial_start) > 0 and len(trial_end) > 0:
+
+            add_extra=False
+            extra_start_idx=[]
+            extra_end_idx=[]
+            extra_start_evt_idx=[]
+            if len(trial_start) > len(trial_end):
+                last_trial_start = trial_start[-1]
+                # Recording goes until end of file
+                dur_step = len(rec_signal) - last_trial_start
+                # Ignore single time step blups
+                if dur_step > 1:
+                    add_extra=True
+                    extra_start_idx=np.max([0, last_trial_start - srate])
+                    extra_end_idx=len(rec_signal) - 1
+                    extra_start_evt_idx=last_trial_start
+                trial_start = trial_start[0:-1]
+            elif len(trial_end) > len(trial_start):
+                first_trial_end = trial_end[0]
+                # Recording starts at beginning of file
+                dur_step = first_trial_end
+                # Ignore single time step blips
+                if dur_step > 1:
+                    trial_start_idx.append(0)
+                    trial_end_idx.append(np.min([len(rec_signal) - 1, first_trial_end + srate]))
+                    trial_start_evt_idx.append(0)
+                trial_end = trial_end[1:]
+
+            # Number of time steps between each up and down state switch
+            dur_steps = trial_end - trial_start
+
+            # For each trial in the file
+            for idx,dur_step in enumerate(dur_steps):
+                # Ignore single time step blups
+                if dur_step > 1:
+                    trial_start_idx.append(np.max([0, trial_start[idx]-srate]))
+                    trial_end_idx.append(np.min([len(rec_signal)-1, trial_end[idx]+srate]))
+                    trial_start_evt_idx.append(trial_start[idx])
+
+            if add_extra:
+                trial_start_idx.append(extra_start_idx)
+                trial_end_idx.append(extra_end_idx)
+                trial_start_evt_idx.append(extra_start_evt_idx)
+
+            seg_trial_start_idx.append(trial_start_idx)
+            seg_trial_end_idx.append(trial_end_idx)
+            seg_trial_start_evt_idx.append(trial_start_evt_idx)
+
+        # If there is a trial start and no trial end
+        elif len(trial_start) > 0 and len(trial_end) == 0:
+            # Recording goes until end of file
+            dur_step = len(rec_signal) - trial_start
+            # Ignore single time step blups
+            if dur_step > 1:
+                seg_trial_start_idx.append([np.max([0,trial_start[0]-srate])])
+                seg_trial_end_idx.append([len(rec_signal)-1])
+                seg_trial_start_evt_idx.append([trial_start[0]])
+        # If there is a trial end and no trial start
+        elif len(trial_start) == 0 and len(trial_end) > 0:
+            # Recording starts at beginning of file
+            dur_step = trial_end
+            # Ignore single time step blips
+            if dur_step > 1:
+                seg_trial_start_idx.append([0])
+                seg_trial_end_idx.append([np.min([len(rec_signal)-1, trial_end[0]+srate])])
+                seg_trial_start_evt_idx.append([0])
+        else:
+            seg_trial_start_idx.append([])
+            seg_trial_end_idx.append([])
+            seg_trial_start_evt_idx.append([])
+
+    # Import spikes
+    for array_idx, region in enumerate(cfg['arrays']):
+
+        fnames = glob(os.path.join(spike_data_dir, 'array_%d' % array_idx, '%s*spikes.csv' % region))
+        for fname in fnames:
+            electrode_df = pd.read_csv(fname)
+            new_data={'array':[], 'electrode':[], 'trial':[], 'time':[]}
+
+            trial_idx=0
+
+            for seg_idx in np.unique(electrode_df.segment):
+                seg_rows = np.where(electrode_df.segment == seg_idx)[0]
+                seg_spike_idx = np.int64(electrode_df.time[seg_rows])
+
+                trial_start_idx=seg_trial_start_idx[seg_idx]
+                trial_end_idx=seg_trial_end_idx[seg_idx]
+                trial_start_evt_idx=seg_trial_start_evt_idx[seg_idx]
+
+                for t_idx in range(len(trial_start_idx)):
+                    trial_rows=np.where((seg_spike_idx>=trial_start_idx[t_idx]) &
+                                        (seg_spike_idx<=trial_end_idx[t_idx]))[0]
+                    trial_spike_times=seg_times[seg_idx][seg_spike_idx[trial_rows]]
+                    trial_start_time=seg_times[seg_idx][int(trial_start_evt_idx[t_idx])]
+                    spike_times=trial_spike_times-trial_start_time
+
+                    new_data['array'].extend(electrode_df.array[seg_rows[trial_rows]])
+                    new_data['electrode'].extend(electrode_df.electrode[seg_rows[trial_rows]])
+                    new_data['trial'].extend((trial_idx*np.ones(len(trial_rows))).tolist())
+                    new_data['time'].extend(spike_times)
+                    trial_idx=trial_idx+1
+            df = pd.DataFrame(new_data, columns=['array', 'electrode', 'trial', 'time'])
+            df.to_csv(os.path.join(out_dir,os.path.split(fname)[1]),index=False)
+
+
+if __name__=='__main__':
+    subject = sys.argv[1]
+    recording_date = sys.argv[2]
+    run_process_multiunit(subject, recording_date)
+    #rerun(subject, recording_date)
