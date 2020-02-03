8323ab42ea8593fcc0aa30f7e372b82e1d8694da
jbonaiuto <jbonaiuto@gmail.com>
2020-02-03 12:41:28 +0100

    parallelize binning

--- src/matlab/spike_data_processing/bin_spikes.m
+++ src/matlab/spike_data_processing/bin_spikes.m
@@ -39,53 +39,69 @@ for f = fieldnames(defaults)',
     end
 end
 
+tic
+
 % Time of baseline event in each trial
 baseline_evt_times=data.metadata.(params.baseline_evt);
 
 % Add new fields to data
 data.bins=[woi(1):binwidth:woi(2)];
 data.baseline_bins=[params.baseline_woi(1):binwidth:params.baseline_woi(2)];
-data.binned_spikes=zeros(length(data.arrays), length(data.electrodes),...
+binned_spikes=zeros(length(data.arrays), length(data.electrodes),...
     data.ntrials, length(data.bins));
-data.binned_baseline_spikes=zeros(length(data.arrays),...
+binned_baseline_spikes=zeros(length(data.arrays),...
     length(data.electrodes), data.ntrials, length(data.baseline_bins));
 
 % Bin all spikes from all trials, electrodes, and arrays
-[binned_spikes,bin_idx]=histc(data.spikedata.time,data.bins);
+[~,bin_idx]=histc(data.spikedata.time,data.bins);
 % Realign spikes to baseline event times and bin all spikes from all
 % trials, electrodes, and arrays
 bc_aligned_spikes=data.spikedata.time-baseline_evt_times(data.spikedata.trial);
-[baseline_binned_spikes,baseline_bin_idx]=histc(bc_aligned_spikes,data.baseline_bins);
+[~,baseline_bin_idx]=histc(bc_aligned_spikes,data.baseline_bins);
+
+arrays=data.arrays;
+spikedata_array=data.spikedata.array;
+electrodes=data.electrodes;
+spikedata_electrode=data.spikedata.electrode;
+ntrials=data.ntrials;
+spikedata_trial=data.spikedata.trial;
 
 % For each array
 for a_idx=1:length(data.arrays)
     % Find rows for this array in list of spikes
-    array_idx=data.arrays(a_idx);
-    array_rows=data.spikedata.array==array_idx;
+    array_idx=arrays(a_idx);
+    array_rows=spikedata_array==array_idx;
     
     % For each electrode
-    for e_idx=1:length(data.electrodes)
+    parfor e_idx=1:length(electrodes)
         
+        electrode_binned_spikes=zeros(data.ntrials, length(data.bins));
+        electrode_binned_baseline_spikes=zeros(data.ntrials, length(data.baseline_bins));
+    
         % Find rows for this electrode in list of spikes
-        electrode_rows=array_rows & data.spikedata.electrode==e_idx;
+        electrode_rows=array_rows & spikedata_electrode==e_idx;
         
         % For each trial
-        for t_idx=1:data.ntrials
+        for t_idx=1:ntrials
             
             % Find rows for this trial in list of spikes
-            trial_rows= electrode_rows & data.spikedata.trial==t_idx;
+            trial_rows= electrode_rows & spikedata_trial==t_idx;
             
             % Add to binned_spikes based on bin_idx
             trial_bins=bin_idx(trial_rows);
             [uvals, ~, uidx] = unique(trial_bins(trial_bins>0)');
-            data.binned_spikes(a_idx,e_idx,t_idx,uvals)=accumarray(uidx, 1);
+            electrode_binned_spikes(t_idx,uvals)=accumarray(uidx, 1);
             
             % Add to binned_baseline_spikes based on baseline_bin_idx
             trial_bins=baseline_bin_idx(trial_rows);
             [uvals, ~, uidx] = unique(trial_bins(trial_bins>0)');
-            data.binned_baseline_spikes(a_idx,e_idx,t_idx,uvals)=accumarray(uidx, 1);
+            electrode_binned_baseline_spikes(t_idx,uvals)=accumarray(uidx, 1);
         end        
-    end
+        binned_spikes(a_idx,e_idx,:,:)=electrode_binned_spikes;
+        binned_baseline_spikes(a_idx,e_idx,:,:)=electrode_binned_baseline_spikes;
+    end    
 end
+data.binned_spikes=binned_spikes;
+data.binned_baseline_spikes=binned_baseline_spikes;
 
-
+toc
