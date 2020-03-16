7ef8f7275106ed0d80016ef2330f6e43988e079a
James Bonaiuto <jbonaiuto@gmail.com>
2020-03-16 14:31:53 +0100

    commented, refactoring

--- src/matlab/hmm/simulation/create_simulated_data.m
+++ src/matlab/hmm/simulation/create_simulated_data.m
@@ -1,14 +1,39 @@
 function [trial_data, state_rates, state_seq_vecs]=create_simulated_data(params)
+% CREATE_SIMULATED_DATA Crate simulated data for HMM
+%
+% Syntax: [trial_data, state_rates, state_seq_vecs]=create_simulated_data(params)
+%
+% Inputs:
+%    params - structure of simulaton parameters
+%
+% Outputs:
+%    trial_data - cell array with a matrix of spikes for each trial
+%    state_rates - for each state, firing rate of each neuron
+%    state_seq_vecs - cell array - the state index for each time point in
+%        trial, for each trial
+% 
+% Example:
+%     [trial_data, state_rates, state_seq_vecs]=create_simulated_data(params)
 
+% Initialize firing rate for each neuron in each state
 state_rates = ceil(rand(params.nNeurons,params.nActualStates)*params.maxFR);
+
+% Initialize trial spikes and state sequence vectors
 trial_data={};
-stateSeqs={};
 state_seq_vecs={};
-state_seq_durs={};
+
+% For each trial
 for i=1:params.nTrials
-    stateDurMeans = normrnd(1,.1,1,params.nActualStates);
-    stateDurStds = .2*normrnd(1,.2,1,params.nActualStates);
-    stateSeqs{i} = ceil(rand(1,params.stateSeqLength)*params.nActualStates);    
-    [trial_data{i},state_seq_durs{i},state_seq_vecs{i}] = genPoissSpikes(state_rates,...
-        stateSeqs{i},params.dt,stateDurMeans,stateDurStds,params.noise_level);
+    
+    % Generate random state mean and std dev durations
+    state_dur_means = normrnd(1,.1,1,params.nActualStates);
+    state_dur_stds = .2*normrnd(1,.2,1,params.nActualStates);
+    
+    % Generate random state sequences
+    state_seq = ceil(rand(1,params.stateSeqLength)*params.nActualStates);    
+    
+    % Generate trial spikes and state sequence vector
+    [trial_data{i},state_seq_vecs{i}] = gen_poiss_spikes(state_rates,...
+        state_seq, params.dt, state_dur_means, state_dur_stds,...
+        params.noise_level);
 end
