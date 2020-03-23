cc87690bd687b339357c967841fc78763b1ad5b7
Merluin <57815781+Merluin@users.noreply.github.com>
2020-03-23 20:11:36 +0100

    resolve fix

--- src/matlab/functional_connectivity/granger_glm/WorkingScript_fix.m
+++ src/matlab/functional_connectivity/granger_glm/WorkingScript_fix.m
@@ -26,51 +26,5 @@ datapath = '/Users/quettier/Desktop/multiunit_binned_data/betta'; % data folder 
 addpath('C:\Users\quettier\Documents\GitHub\tool_learning\src\matlab\spm12'); 
 
 
-%% saving data
-for week_number = 1:2
-dates = week(week_number); 
-
-parentdir = '/Users/quettier/Desktop/multiunit_binned_data/output/functional_connectivity';
- if condition == 1
-        foldername = sprintf('Week_%d_motor_%s', week_number,period); 
-    elseif condition == 0      
-        foldername = sprintf('Week_%d_visual_%s',week_number,period);
-    end
-newfolder = fullfile(parentdir, foldername);
-    if ~exist(newfolder, 'dir')
-       mkdir(newfolder)
-    end
-output = sprintf('%s/%s',parentdir,foldername);
-
-
-%% function
-% full trial
-if condition == 0
-    conditions={'visual_grasp_left','visual_grasp_right'};
-elseif condition == 1
-    conditions={'motor_grasp_left','motor_grasp_center','motor_grasp_right'};
-end
-
-func_connectivity_within_array(datapath, dates, arrays, electrode, conditions,period, 'output_path', output)
-
-end
-% 0 sec  to 2 sec
-%     if condition == 1
-%         foldername = sprintf('Week_%d_motor_Z', week_number); 
-%     elseif condition == 0      
-%         foldername = sprintf('Week_%d_visual_Z',week_number);
-%     end
-% newfolder = fullfile(parentdir, foldername);
-%     if ~exist(newfolder, 'dir')
-%        mkdir(newfolder)
-%     end
-% output = sprintf('%s/%s',parentdir,foldername);
-% 
-% func_connectivity_within_array_Z(datapath, dates, arrays, electrode, conditions, 'output_path', output)
-
-% file plot
-weeks=[1:14 16:24];
-weeks=[1:2];
-plot_table(weeks,'visual_fix_on', 'input_path',parentdir, 'output_path',parentdir);
 
 %% END
