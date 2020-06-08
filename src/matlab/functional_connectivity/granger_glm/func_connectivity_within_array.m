bfd9784273baef96cd47442fd489e23fedf3f783
Merluin <57815781+Merluin@users.noreply.github.com>
2020-06-08 12:13:01 +0200

    init_exp_info_bug_fixed

--- src/matlab/functional_connectivity/granger_glm/func_connectivity_within_array.m
+++ src/matlab/functional_connectivity/granger_glm/func_connectivity_within_array.m
@@ -26,7 +26,9 @@ for f=fieldnames(defaults)'
 end
 
 addpath('../../spike_data_processing');
+ addpath('../../../matlab');
 
+exp_info= init_exp_info();
 
 
 M={};
@@ -40,7 +42,7 @@ for array_idx=1:length(array)
         date=dates{date_idx};
         load(fullfile(data_dir, date, 'multiunit','binned',sprintf('fr_b_%s_%s_%s.mat', array{array_idx}, date,event)));
          data=concatenate_data({data});
-        data=filter_data(exp_info, data,'thresh_percentile' );
+        data=filter_data(exp_info, data,'thresh_percentile',10 );
         
         date_data{date_idx}=data;
     end
