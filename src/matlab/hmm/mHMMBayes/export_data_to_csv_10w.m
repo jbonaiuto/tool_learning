e1d571d059f2a2f8aec1e6efc4118bfbaeb2c5a8
Seb Kirchherr <sebastien.kirchherr@gmail.com>
2021-02-16 12:07:21 +0100

    initial import

--- src/matlab/hmm/mHMMBayes/export_data_to_csv_10w.m
+++ src/matlab/hmm/mHMMBayes/export_data_to_csv_10w.m
@@ -1,116 +1,127 @@
-function data=export_data_to_csv_10w(exp_info, subject, array, conditions, dates, dt, output_path)
+function concat_data=export_data_to_csv_10w(exp_info, subject, array, conditions, dates, dt, output_path)
 
-%% Load and concatenate spike data
+% Load and concatenate spike data
 addpath('../../spike_data_processing');
 
 dbstop if error
 
 data_file=fullfile(output_path, 'hmm_data.csv');
-    fid=fopen(data_file,'w');
+fid=fopen(data_file,'w');
     header='date,trial,condition,electrode,timestep,value';
     fprintf(fid,'%s\n',header);
 
-anchor=0;
-anchor_length=0;
-
+all_data={};
 for d_idx=1:length(dates)
     date=dates{d_idx};
 
     load(fullfile(exp_info.base_data_dir, 'preprocessed_data', subject,...
         date,'multiunit','binned',...
         sprintf('fr_b_%s_%s_whole_trial.mat',array,date)));
-    data.electrodes=[1,2,3,4,5,6,7,9,13,18,25,27,29,31,32];
+    %data.electrodes=[1,2,3,4,5,6,7,9,13,18,25,27,29,31,32];
 
     data.trial_date=ones(1,data.ntrials);
+    %data.name = ['data_', num2str(d_idx)];
 
     % Filter data - RTs too fast or slow
     data=filter_data(exp_info,data,'plot_corrs',true,'thresh_percentile', 10);
     data=rebin_spikes(data,dt);
 
     % Use data from "good" electrodes
-    good_electrodes=[1,2,3,4,5,6,7,9,13,18,25,27,29,31,32];
-    data.binned_spikes=data.binned_spikes(:,good_electrodes,:,:);
-    data.binned_baseline_spikes=data.binned_baseline_spikes(:,good_electrodes,:,:);
-    data.smoothed_firing_rate=data.smoothed_firing_rate(:,good_electrodes,:,:);
+%     good_electrodes=[1,2,3,4,5,6,7,9,13,18,25,27,29,31,32];
+%     data.binned_spikes=data.binned_spikes(:,good_electrodes,:,:);
+%     data.binned_baseline_spikes=data.binned_baseline_spikes(:,good_electrodes,:,:);
+%     data.smoothed_firing_rate=data.smoothed_firing_rate(:,good_electrodes,:,:);
 
 
-    %% Figure out which trials to use and get trial data
-    trials=zeros(1,length(data.metadata.condition));
-    for i=1:length(conditions)
-        trials = trials | (strcmp(data.metadata.condition,conditions{i}));
-    end
+     data.binned_spikes=data.binned_spikes(:,:,:,:);
+     data.binned_baseline_spikes=data.binned_baseline_spikes(:,:,:,:);
+     data.smoothed_firing_rate=data.smoothed_firing_rate(:,:,:,:);
 
-    trials=find(trials);
-    trial_date=data.trial_date(trials);
-    trial_condition=data.metadata.condition(trials);
+    %export the csv file just for the first loop in runHMM_grasp_10w (i=1:length(dates)
+    all_data{d_idx}=data;
+end
+concat_data=concatenate_data(all_data, 'spike_times',false);
+clear all_data;
 
-    cond_data=squeeze(data.binned_spikes(1,:,trials,:));
+% Figure out which trials to use and get trial data
+trials=zeros(1,length(concat_data.metadata.condition));
+for i=1:length(conditions)
+    trials = trials | (strcmp(concat_data.metadata.condition,conditions{i}));
+end
 
-    trial_spikes={};
+trials=find(trials);
+trial_date=concat_data.trial_date(trials);
+trial_condition=concat_data.metadata.condition(trials);
 
-    max_n_bins=0;
+cond_data=squeeze(concat_data.binned_spikes(1,:,trials,:));
 
-    %% Get trial spikes
-    for g = 1:length(trials)
-        % Get binned spikes for this trial from time 0 to time of reward
-        trial_idx = trials(g);
-        bin_idx=find((data.bins>=0) & (data.bins<=(data.metadata.place(trial_idx)+150)));
-        trial_data=squeeze(cond_data(:,g,bin_idx));
-        trial_spikes{end+1}=trial_data;
-        max_n_bins=max([max_n_bins size(trial_data,2)]);
-    end
+trial_spikes={};
+
+max_n_bins=0;
+
+% Get trial spikes
+for g = 1:length(trials)
+    % Get binned spikes for this trial from time 0 to time of reward
+    trial_idx = trials(g);
+    bin_idx=find((concat_data.bins>=0) & (concat_data.bins<=(concat_data.metadata.place(trial_idx)+150)));
+    trial_data=squeeze(cond_data(:,g,bin_idx));
+    trial_spikes{end+1}=trial_data;
+    max_n_bins=max([max_n_bins size(trial_data,2)]);
+end
+
+
+fid=fopen(fullfile(output_path,'hmm_data.csv'),'w');
+header='date,trial,condition,electrode,timestep,value';
+fprintf(fid,'%s\n',header);
+
+%loop without conditions
+% for i=1:length(dates)
+%     day_spikes=trial_spikes(trial_date==i);
+%     for j=1:length(day_spikes)
+%         trial=day_spikes{j};
+%         condition_idx=find(strcmp(conditions,trial_condition{j}));
+%         for k=1:size(trial,1)
+%             for l=1:size(trial,2)
+%                 line=sprintf('%d,%d,%d,%d,%d,%d',1,j,condition_idx,k,l,trial(k,l));
+%                 fprintf(fid,'%s\n',line);
+%             end
+%         end
+%     end
+% end
+
+ anchor=0;
+ anchor_length=0;
+    
+        if d_idx==1
+            for j=1:length(trial_spikes)
+                trial=trial_spikes{j};
+                condition_idx=find(strcmp(conditions,trial_condition{j}));
+                for k=1:size(trial,1)
+                    for l=1:size(trial,2)
+                        line=sprintf('%d,%d,%d,%d,%d,%d',1,j,condition_idx,k,l,trial(k,l));
+                        fprintf(fid,'%s\n',line);
+
+                     end
+                end
+            anchor_length=anchor_length+1;
+            end
+        anchor = anchor_length;
+        else
+        anchor_length=0;
+            for j=1:length(trial_spikes)
+                trial=trial_spikes{j};
+                condition_idx=find(strcmp(conditions,trial_condition{j}));
+                j=j+anchor;
+                for k=1:size(trial,1)
+                    for l=1:size(trial,2)
+                        line=sprintf('%d,%d,%d,%d,%d,%d',1,j,condition_idx,k,l,trial(k,l));
+                        fprintf(fid,'%s\n',line);
+                     end
+                end 
+            anchor_length=anchor_length+1;
+            end
+        anchor = anchor + anchor_length;  
+        end
   
 
-    % fid=fopen('hmm_data.csv','w');
-    % header='date,trial,electrode,timestep,value';
-    % fprintf(fid,'%s\n',header);
-    % 
-    % %loop without conditions
-    % for i=1:length(dates)
-    %     day_spikes=trial_spikes(trial_date==i);
-    %     for j=1:length(day_spikes)
-    %         trial=day_spikes{j};
-    %         for k=1:size(trial,1)
-    %             for l=1:size(trial,2)
-    %                 line=sprintf('%d,%d,%d,%d,%d',i,j,k,l,trial(k,l));
-    %                 fprintf(fid,'%s\n',line);
-    %             end
-    %         end
-    %     end
-    % end
-
-    %loop with conditions
-    
-    if d_idx==1
-        for j=1:length(trial_spikes)
-            trial=trial_spikes{j};
-            condition_idx=find(strcmp(conditions,trial_condition{j}));
-            for k=1:size(trial,1)
-                for l=1:size(trial,2)
-                    line=sprintf('%d,%d,%d,%d,%d,%d',1,j,condition_idx,k,l,trial(k,l));
-                    fprintf(fid,'%s\n',line);
-                    
-                 end
-            end
-        anchor_length=anchor_length+1;
-        end
-    anchor = anchor_length;
-    else
-    anchor_length=0;
-        for j=1:length(trial_spikes)
-            trial=trial_spikes{j};
-            condition_idx=find(strcmp(conditions,trial_condition{j}));
-            j=j+anchor;
-            for k=1:size(trial,1)
-                for l=1:size(trial,2)
-                    line=sprintf('%d,%d,%d,%d,%d,%d',1,j,condition_idx,k,l,trial(k,l));
-                    fprintf(fid,'%s\n',line);
-                 end
-            end 
-        anchor_length=anchor_length+1;
-        end
-    anchor = anchor + anchor_length;  
-    end
-
-end
 fclose(fid);
--- src/matlab/hmm/mHMMBayes/runHMM_grasp_10w.m
+++ src/matlab/hmm/mHMMBayes/runHMM_grasp_10w.m
@@ -1,5 +1,5 @@
 dbstop if error
-clear all
+%clear all
 
 addpath('../..');
 exp_info=init_exp_info();
@@ -13,10 +13,13 @@ array='F1';
 conditions={'motor_grasp_center','motor_grasp_right','motor_grasp_left'};
 
 %10 weeks model
-    dates={'04.03.19','05.03.19'};%,'07.03.19','08.03.19','11.03.19','13.03.19','14.03.19','15.03.19','19.03.19','20.03.19','21.03.19','22.03.19','25.03.19',...
-    %'26.03.19','27.03.19','28.03.19','29.03.19','01.04.19','02.04.19','05.04.19','08.04.19','09.04.19','10.04.19','12.04.19','15.04.19','16.04.19',...
-    %'17.04.19','18.04.19','19.04.19','23.04.19','24.04.19','26.04.19','03.05.19','07.05.19','09.05.19','13.05.19','15.05.19','16.05.19','17.05.19'};
-    
+dates={'14.02.19','18.02.19','19.02.19','20.02.19','21.02.19','25.02.19','26.02.19','27.02.19','28.02.19','04.03.19','01.03.19','05.03.19','07.03.19',...
+    '08.03.19','11.03.19','13.03.19','14.03.19','15.03.19','19.03.19','20.03.19','21.03.19','22.03.19','25.03.19',...
+    '26.03.19','27.03.19','28.03.19','29.03.19','01.04.19','02.04.19','05.04.19','08.04.19','09.04.19','10.04.19','12.04.19','15.04.19','16.04.19',...
+    '17.04.19','18.04.19','19.04.19','23.04.19','24.04.19','26.04.19','03.05.19','07.05.19','09.05.19','13.05.19','15.05.19','17.05.19','20.05.19','21.05.19'};
+
+%dates={'27.03.19','28.03.19','29.03.19'};
+
 dt=10;
 
 output_path=fullfile(exp_info.base_output_dir, 'HMM', 'betta', 'grasp', '10w_condHMM', array);
@@ -24,14 +27,16 @@ if exist(output_path,'dir')~=7
     mkdir(output_path);
 end
 
-data=export_data_to_csv_10w(exp_info, subject, array, conditions, dates, dt, output_path);
+load('data.mat');
+%data=export_data_to_csv_10w(exp_info, subject, array, conditions, dates, dt, output_path);
 
 %system(sprintf('"C:/Program Files/R/R-3.6.1/bin/Rscript" ../../../R/hmm/fit_10w.R "%s"', strrep(output_path,'\','/')));
+%system(sprintf('"C:/Program Files/R/R-3.6.1/bin/Rscript" ../../../R/hmm/fit.R "%s"', strrep(output_path,'\','/')));
 
-% T = readtable(fullfile(output_path, 'aic.csv'));
-% minAIC=min(T.aic);
-% forward_prob_idx=find(T.aic==minAIC);
-% n_states=T.states(forward_prob_idx);
-% run_idx=T.run(forward_prob_idx);
-% 
-% plotHMM_aligned_condition_grasp(data, date, conditions, array, fullfile(output_path,sprintf('forward_probs_%dstates_%d.csv',n_states,run_idx)));
+T = readtable(fullfile(output_path, 'aic.csv'));
+minAIC=min(T.aic);
+forward_prob_idx=find(T.aic==minAIC);
+n_states=T.states(forward_prob_idx);
+run_idx=T.run(forward_prob_idx);
+
+plotHMM_aligned_condition_grasp(data, '50d', conditions, array, fullfile(output_path,sprintf('forward_probs_%dstates_%d.csv',n_states,run_idx)));
