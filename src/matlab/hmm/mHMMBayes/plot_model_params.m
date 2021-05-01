1d12f0db7e4b4ebfcfc3b4a3306b9b6adb224096
James Bonaiuto <jbonaiuto@gmail.com>
2021-05-01 17:51:58 +0200

    changed plot size, dont hard code number of electrodes

--- src/matlab/hmm/mHMMBayes/plot_model_params.m
+++ src/matlab/hmm/mHMMBayes/plot_model_params.m
@@ -1,21 +1,22 @@
 function plot_model_params(model)
 
-figure();
+f=figure();
+set(f, 'Position', [0 629 1186 245]);
+
 subplot(1,3,1);
 imagesc(model.trans_mat);
-set(gca,'ydir','normal');
 set(gca,'XTick',[1:model.n_states],'XTickLabel',model.metadata.state_labels);
 set(gca,'YTick',[1:model.n_states],'YTickLabel',model.metadata.state_labels);
 colorbar()
 subplot(1,3,2);
 plot(model.emiss_alpha_mat');
 legend(model.metadata.state_labels);
-xlim([1 32]);
+xlim([1 size(model.emiss_alpha_mat,2)]);
 xlabel('Electrode');
 ylabel('Alpha');
 subplot(1,3,3);
 plot(model.emiss_beta_mat');
 legend(model.metadata.state_labels);
-xlim([1 32]);
+xlim([1 size(model.emiss_beta_mat,2)]);
 xlabel('Electrode');
 ylabel('Beta');
