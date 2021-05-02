e105822a9074c2b81a1afd53baa3cd8524c23482
James Bonaiuto <jbonaiuto@gmail.com>
2021-05-02 13:52:23 +0200

    plot log of transition probs

--- src/matlab/hmm/mHMMBayes/plot_model_params.m
+++ src/matlab/hmm/mHMMBayes/plot_model_params.m
@@ -4,10 +4,11 @@ f=figure();
 set(f, 'Position', [0 629 1186 245]);
 
 subplot(1,3,1);
-imagesc(model.trans_mat);
+imagesc(log(model.trans_mat));
 set(gca,'XTick',[1:model.n_states],'XTickLabel',model.metadata.state_labels);
 set(gca,'YTick',[1:model.n_states],'YTickLabel',model.metadata.state_labels);
-colorbar()
+a=colorbar();
+ylabel(a,'log(prob)');
 subplot(1,3,2);
 plot(model.emiss_alpha_mat');
 legend(model.metadata.state_labels);
