e53a38c9dced81aed0746cdfa92b0cae4f22f761
James Bonaiuto <bonaiuto@n18h202.isc.cnrs.fr>
2021-05-07 12:20:39 +0200

    simplified trial indexing

--- src/matlab/hmm/mHMMBayes/plot_fwd_probs_event_sorted.m
+++ src/matlab/hmm/mHMMBayes/plot_fwd_probs_event_sorted.m
@@ -14,54 +14,40 @@ all_t_idx=1;
 w=gausswin(10);
 w=w/sum(w);
 
-% For each condition
-for cond_idx=1:length(conditions)
-    % Find data trials for this condition
-    condition_trials = find(strcmp(data.metadata.condition,conditions{cond_idx}));
+% Date index of each trial for this condition
+trial_date=data.trial_date;
     
-    % Date index of each trial for this condition
-    trial_date=data.trial_date(condition_trials);
-    
-    % For every date
-    for d=1:length(dates)
-        % Find trials from this date for this condition
-        day_trials=condition_trials(trial_date==d);
+% For every date
+for d=1:length(dates)
+    % Find trials from this date for this condition
+    day_trials=find(trial_date==d);
             
-        % For each trial from this day in this condition
-        for n=1:length(day_trials)
+    % For each trial from this day in this condition
+    for n=1:length(day_trials)
                 
-            % Rows of forward probabilities for this trial
-            trial_rows=find((model.forward_probs.subj==day_trials(n)));
-            if strcmp(model.type,'multilevel')
-                trial_rows=find((model.forward_probs.subj==d) & (model.forward_probs.rm==n));
-            end
-                            
-            % Get the bins that we used in the HMM (time>0 and up to reward)
-            bin_idx=find((data.bins>=0) & (data.bins<=data.metadata.reward(day_trials(n))));
-            sub_bin_idx=find(data.bins(bin_idx)-data.metadata.go(day_trials(n))>=-500);
-            %trial_times=data.bins(bin_idx);
-            % Find time of alignment event in this trial
-            go_times(all_t_idx) = data.metadata.go(day_trials(n))-data.metadata.go(day_trials(n));
-            mo_times(all_t_idx) = data.metadata.hand_mvmt_onset(day_trials(n))-data.metadata.go(day_trials(n));
-	    oc_times(all_t_idx) = data.metadata.obj_contact(day_trials(n))-data.metadata.go(day_trials(n));
-	    pl_times(all_t_idx) = data.metadata.place(day_trials(n))-data.metadata.go(day_trials(n));
-    
-            % Save p states within this window
-            for i=1:model.n_states
-                sprobs=model.forward_probs.(sprintf('fw_prob_S%d',i));
-                trial_fwd_probs = sprobs(trial_rows);                  
-                trial_state_probs(i,all_t_idx,1:length(sub_bin_idx))=conv(trial_fwd_probs(sub_bin_idx),w,'same');
-                %mask=(trial_fwd_probs>.8);
-                %starts = strfind([0 mask'], [0 1 1 1 1 1 1]);
-                %stops = strfind([mask' 0], [1 1 1 1 1 1 0]);
-                %if length(starts)>0
-                %    state_onset_times(all_t_idx,i)=trial_times(starts(end));%-go_times(condition_trials(t_idx));
-                %else
-                %    state_onset_times(all_t_idx,i)=NaN;
-                %end
-            end
-            all_t_idx=all_t_idx+1;
+        % Rows of forward probabilities for this trial
+        trial_rows=find((model.forward_probs.subj==day_trials(n)));
+        if strcmp(model.type,'multilevel')
+            trial_rows=find((model.forward_probs.subj==d) & (model.forward_probs.rm==n));
         end
+                            
+        % Get the bins that we used in the HMM (time>0 and up to reward)
+        bin_idx=find((data.bins>=0) & (data.bins<=data.metadata.reward(day_trials(n))));
+        sub_bin_idx=find(data.bins(bin_idx)-data.metadata.go(day_trials(n))>=-500);
+    
+        % Find time of alignment event in this trial
+        go_times(all_t_idx) = data.metadata.go(day_trials(n))-data.metadata.go(day_trials(n));
+        mo_times(all_t_idx) = data.metadata.hand_mvmt_onset(day_trials(n))-data.metadata.go(day_trials(n));
+	oc_times(all_t_idx) = data.metadata.obj_contact(day_trials(n))-data.metadata.go(day_trials(n));
+	pl_times(all_t_idx) = data.metadata.place(day_trials(n))-data.metadata.go(day_trials(n));
+    
+        % Save p states within this window
+        for i=1:model.n_states
+            sprobs=model.forward_probs.(sprintf('fw_prob_S%d',i));
+            trial_fwd_probs = sprobs(trial_rows);                  
+            trial_state_probs(i,all_t_idx,1:length(sub_bin_idx))=conv(trial_fwd_probs(sub_bin_idx),w,'same');                
+        end
+        all_t_idx=all_t_idx+1;
     end
 end
 
