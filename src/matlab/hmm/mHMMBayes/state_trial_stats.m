69390b9017f26982d710fe69f4db17a79e2d0c85
Seb Kirchherr <sebastien.kirchherr@gmail.com>
2021-06-02 19:01:31 +0200

    add lifetime plots, not clean I ll work on it after the fractional occupancy

--- src/matlab/hmm/mHMMBayes/state_trial_stats.m
+++ src/matlab/hmm/mHMMBayes/state_trial_stats.m
@@ -37,12 +37,11 @@ for cond_idx=1:length(conditions)
             active_mat_cond(sc,tc)=length(state_onsets_cond{sc,tc});
         end
    
-        % THERES A FUNCTION FOR THIS
-        %mean_active_mat_cond{cond_idx,sc}=sum(active_mat_cond(sc,:),2)/length(condition_trials);
         mean_active_mat_cond(cond_idx,:)=mean(active_mat_cond,2);
     end
 end
 
+%%
 figure()
 center=mean_active_mat_cond(1,:);
 right=mean_active_mat_cond(2,:);
@@ -59,21 +58,145 @@ ylabel('Mean number of activation')
 ylim([0 ceil(max(mean_active_mat_cond(:)))])
 legend('Center','Right','Left','State mean','Location','northeastoutside')
 
+%%
+%average life time of each states
+
+%check if the number of onset is equal to the number of offset in each trials   
+ for s=1:model.n_states
+     for t=1:data.ntrials
+     onsetoffset(s,t)=isequal(length(state_trial_stats.state_onsets{s,t}),length(state_trial_stats.state_offsets{s,t}));
+     end
+ end
+ 
+if find(onsetoffset==0)>0
+    display('check onsetoffset!')
+end
 
 
+lifetime={};
+for s=1:model.n_states
+     for t=1:data.ntrials
+        offset=state_trial_stats.state_offsets{s,t};
+        onset=state_trial_stats.state_onsets{s,t};
+        offon_nbr=length(offset);
+        LT_idx=zeros(1,offon_nbr);
+        for o=1:offon_nbr
+            LT_idx(o)=offset(o)-onset(o);    
+        end
+        lifetime{s,t}=LT_idx;
+     end  
+ end
+
+%mean number of time a state is active for less than 10ms
+state_blip=zeros(model.n_states,data.ntrials);
+for s=1:model.n_states
+     for t=1:data.ntrials
+     state_blip(s,t)=length(find(lifetime{s,t}==0));
+     end
+ end
+mean_state_blip=mean(state_blip,2);
 
 
+%mean state lifetime based on the maximum activation length of a trial
+%(with 0ms duration)
+for s=1:model.n_states
+     for t=1:data.ntrials
+        if isempty(lifetime{s,t})==1
+           lifetime{s,t}=0;
+        end
+     end
+ end
 
+LT_max=zeros(model.n_states,data.ntrials);
+for s=1:model.n_states
+     for t=1:data.ntrials
+     LT_max(s,t)=max(lifetime{s,t});
+     end
+ end
+mean_LT_max=mean(LT_max,2);
 
+for cond_idx=1:length(conditions)
+    condition_trials = find(strcmp(data.metadata.condition,conditions{cond_idx}));
+    LT_max_cond=zeros(model.n_states,length(condition_trials));
+    for sc=1:model.n_states
+        lifetime_cond=lifetime(:,condition_trials);
+        for tc=1:length(condition_trials)
+            LT_max_cond(sc,tc)=max(lifetime_cond{sc,tc});
+        end
+   
+        mean_LT_max_cond(cond_idx,:)=mean(LT_max_cond,2);
+    end
+end
 
+        %mean state lifetime based on the maximum activation length of a trial
+        %(WITHOUT 0ms duration)
+        for s=1:model.n_states
+            state=LT_max(s,:);
+            state_zeros=find(LT_max(s,:)==0);
+            state([state1_zeros])=[];
+            mean_LT_max_no0(s)=mean(state,2);
+        end    
+        mean_LT_max_no0=mean_LT_max_no0';
 
+        %%
+figure()
+center=mean_LT_max_cond(1,:);
+right=mean_LT_max_cond(2,:);
+left=mean_LT_max_cond(3,:);
 
+bar_cond=bar(1:model.n_states, [center' right' left'], 1);
 
+hold on
+plot(mean_LT_max,'Marker','s','LineStyle','none','MarkerSize',10,'MarkerFaceColor','k');
 
+title('Mean maximum lifetime per trial')
+xlabel('States')
+ylabel('Duration (ms)')
+ylim([0 ceil(max(mean_LT_max_cond(:)))])
+legend('Center','Right','Left','State mean','Location','northeastoutside')
 
+%%
 
+%mean state lifetime based on the sum of all activations of a trial
+LT_sum=zeros(model.n_states,data.ntrials);
+for s=1:model.n_states
+     for t=1:data.ntrials
+     LT_sum(s,t)=sum(lifetime{s,t});
+     end
+ end
+mean_LT_sum=mean(LT_sum,2);
 
+for cond_idx=1:length(conditions)
+    condition_trials = find(strcmp(data.metadata.condition,conditions{cond_idx}));
+    LT_sum_cond=zeros(model.n_states,length(condition_trials));
+    for sc=1:model.n_states
+        lifetime_cond=lifetime(:,condition_trials);
+        for tc=1:length(condition_trials)
+            LT_sum_cond(sc,tc)=sum(lifetime_cond{sc,tc});
+        end
+   
+        mean_LT_sum_cond(cond_idx,:)=mean(LT_sum_cond,2);
+    end
+end
 
+%%
+figure()
+center=mean_LT_sum_cond(1,:);
+right=mean_LT_sum_cond(2,:);
+left=mean_LT_sum_cond(3,:);
+
+bar_cond=bar(1:model.n_states, [center' right' left'], 1);
+
+hold on
+plot(mean_LT_sum,'Marker','s','LineStyle','none','MarkerSize',10,'MarkerFaceColor','k');
+
+title('Mean sum lifetime per trial')
+xlabel('States')
+ylabel('Mean duration')
+ylim([0 ceil(max(mean_LT_sum_cond(:)))])
+legend('Center','Right','Left','State mean','Location','northeastoutside')
+
+%%
 
 
 
