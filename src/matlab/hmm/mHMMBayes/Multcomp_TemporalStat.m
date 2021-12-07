76fa89cf487c7a0277c6b2084b0bc9ec8d0e4f2c
Seb Kirchherr <sebastien.kirchherr@gmail.com>
2021-12-07 16:34:59 +0100

    better results statement print

--- src/matlab/hmm/mHMMBayes/Multcomp_TemporalStat.m
+++ src/matlab/hmm/mHMMBayes/Multcomp_TemporalStat.m
@@ -65,36 +65,61 @@ for s_idx=1:model.n_states
     eval(['active_S' num2str(s_idx) 'C3=active_mat_cond_state(find(active_mat_state==s_idx & active_mat_cond==3));']); 
 end
 
-state1={};
-state2={};
-pValue={};
+% state1={};
+% state2={};
+% pValue={};
+results_stmt=fopen(fullfile(output_path,'ANOVA_ResultStatement_Activation_2 states.txt'),'wt');
 
 for s_idx=1:model.n_states-1
         for ss_idx=s_idx+1:model.n_states
             anova_State_mat(:,1)=eval(['active_S' num2str(s_idx)]);
             anova_State_mat(:,2)=eval(['active_S' num2str(ss_idx)]);
             
-            [p, tbl, stats]=anova1(anova_State_mat);
+            %[p, tbl, stats]=anova1(anova_State_mat);
             
-            state1{end+1}=sprintf('state%d',s_idx);
-            state2{end+1}=sprintf('state%d',ss_idx);
-            pValue{end+1}=p;
+%             anova_grpdf=cell2mat(tbl(2,3));
+%             anova_errordf=cell2mat(tbl(3,3));
+%             anova_F=cell2mat(tbl(2,5));
+%             anova_p=cell2mat(tbl(2,6));
+            STD1=std(rmmissing(anova_State_mat(:,1)));
+            STD2=std(rmmissing(anova_State_mat(:,2)));
+            m1=mean(rmmissing(anova_State_mat(:,1)));
+            m2=mean(rmmissing(anova_State_mat(:,2)));
+            
+%             % test for normality assumtion with Shapiro-wilk test
+%                 [H, pValue, W]=swtest(anova_State_mat(:,2), 0.05);
+            
+            [h,p,ci,stats]=ttest2(anova_State_mat(:,1),anova_State_mat(:,2));
+            
+            group_stmt=sprintf('state%d(mean=%f,SD=%f) state%d(%f,%f) \n',s_idx,m1,STD1,ss_idx,m2,STD2);
+%             anova_stmt=sprintf('ANOVA: F(%d,%d)=%f, p=%f \n',anova_grpdf,anova_errordf, anova_F,anova_p);
+            t_stmt=sprintf('t-test (unpaired): t(df=%d) = %f, SD = %f, CI(%f, %f), p = %f \n',stats.df,stats.tstat,stats.sd,ci(1),ci(2),p);
+
+            fprintf(results_stmt,[group_stmt t_stmt '\n']);
+            
+%             stats=struct2table(stats,'AsArray',true);
+%             writetable(stats,fullfile(output_path,sprintf('ANOVAstats_activation_state%d_state%d.txt',s_idx,ss_idx)),'Delimiter','\t');
+%             
+%             state1{end+1}=sprintf('state%d',s_idx);
+%             state2{end+1}=sprintf('state%d',ss_idx);
+%             pValue{end+1}=p;
         end
 end
 
-state1=state1';
-state2=state2';
-pValue=pValue';
-
-T2states_activation=table(state1,state2,pValue);
-
-writetable(T2states_activation,fullfile(output_path,'T2states_activation.txt'),'Delimiter','\t');
+% state1=state1';
+% state2=state2';
+% pValue=pValue';
+% 
+% T2states_activation=table(state1,state2,pValue);
+% 
+% writetable(T2states_activation,fullfile(output_path,'T2states_activation.txt'),'Delimiter','\t');
 
 %between condition comparison
-state={};
-condition={};
-pValue={};
-
+% state={};
+% pValue={};
+% condition1={};
+% condition2={};
+results_stmt=fopen(fullfile(output_path,'ANOVAHSD_ResultStatement_Activation_3conditions.txt'),'wt');
 for s_idx=1:model.n_states
     for t_idx=1:cond_trial_nbr(1)
         anova_State_cond_mat(t_idx,1)=eval(['active_S' num2str(s_idx) 'C1(t_idx)']);
@@ -106,23 +131,69 @@ for s_idx=1:model.n_states
         anova_State_cond_mat(t_idx,3)=eval(['active_S' num2str(s_idx) 'C3(t_idx)']);
     end
     
+%     % test for normality assumtion with Shapiro-wilk test
+%             [H, pValue, W]=swtest(anova_State_cond_mat(:,3), 0.05);
+    
     [p, tbl, stats]=anova1(anova_State_cond_mat);
-    [c,m,h,gnames]=multcompare(stats,'dimension',[1 2]);
+    
+    [c,m,h,gnames]=multcompare(stats,'dimension',[1 2],'Display','off');
 
+    size1=stats.n(1);
+    size2=stats.n(2);
+    size3=stats.n(3);
+    m1=m(1,1);
+    m2=m(2,1);
+    m3=m(3,1);
+    SE1=m(1,2);
+    SE2=m(2,2);
+    SE3=m(3,2);
+    HSD_lowCI_12=c(1,3);
+    HSD_upCI_12=c(1,5);
+    HSD_lowCI_13=c(2,3);
+    HSD_upCI_13=c(2,5);
+    HSD_lowCI_23=c(3,3);
+    HSD_upCI_23=c(3,5);
+    STD1=std(rmmissing(anova_State_cond_mat(:,1)));
+    STD2=std(rmmissing(anova_State_cond_mat(:,2)));
+    STD3=std(rmmissing(anova_State_cond_mat(:,3)));
+    anova_grpdf=cell2mat(tbl(2,3));
+    anova_errordf=cell2mat(tbl(3,3));
+    anova_F=cell2mat(tbl(2,5));
+    anova_p=cell2mat(tbl(2,6));
+    HSD_p=c(1,6);
+    
+    %results statement
+    
+    
+    group_stmt=sprintf('state%d  cond1(mean=%f, SD=%f, SE=%f)  cond2(%f, %f, %f)  cond3(%f, %f, %f) \n',s_idx,m1,STD1,SE1,m2,STD2,SE2,m3,STD3,SE3);
+    anova_stmt=sprintf('ANOVA: F(%d,%d) = %f,  p = %f \n',anova_grpdf,anova_errordf, anova_F,anova_p);
+    fprintf(results_stmt,[group_stmt anova_stmt]);
     for c_idx=1:length(conditions)
-        state{end+1}=sprintf('state%d',s_idx);
-        condition{end+1}=sprintf('condition%d',c_idx);
-        pValue{end+1}=c(c_idx,6);
+        TukeyHSD_stmt=sprintf('Tukey-HSD: cond%d / cond%d,  CI(%f, %f),  p = %f \n',c(c_idx,1),c(c_idx,2),c(c_idx,3),c(c_idx,5),c(c_idx,6));
+        fprintf(results_stmt,[TukeyHSD_stmt '\n']);
     end
+    fprintf(results_stmt,'\n');
+    
+    
+%     stats=struct2table(stats,'AsArray',true);
+%     writetable(stats,fullfile(output_path,sprintf('ANOVAstats_activation_state%d_3conditions.txt',s_idx)),'Delimiter','\t');
+%     
+%     for c_idx=1:length(conditions)
+%         state{end+1}=sprintf('state%d',s_idx);
+%         condition1{end+1}=sprintf('condition%d',c(c_idx,1));
+%         condition2{end+1}=sprintf('condition%d',c(c_idx,2));
+%         pValue{end+1}=c(c_idx,6);
+%     end
 end
 
-state=state';
-condition=condition';
-pValue=pValue';
-
-T1state3cond_activation=table(state,condition,pValue);
-
-writetable(T1state3cond_activation,fullfile(output_path,'T1state3cond_activation.txt'),'Delimiter','\t');
+% state=state';
+% condition1=condition1';
+% condition2=condition2';
+% pValue=pValue';
+% 
+% T1state3cond_activation=table(state,condition1,condition2,pValue);
+% 
+% writetable(T1state3cond_activation,fullfile(output_path,'T1state3cond_activation.txt'),'Delimiter','\t');
 
 %% Life time calculated based on the max duration of a state 
 
@@ -181,6 +252,8 @@ for s_idx=1:model.n_states-1
             anova_State_mat(:,2)=eval(['LT_max_S' num2str(ss_idx)]);
             
             [p, tbl, stats]=anova1(anova_State_mat);
+             stats=struct2table(stats,'AsArray',true);
+            writetable(stats,fullfile(output_path,sprintf('ANOVAstats_LifeTimeMax_state%d_state%d.txt',s_idx,ss_idx)),'Delimiter','\t');
             
             state1{end+1}=sprintf('state%d',s_idx);
             state2{end+1}=sprintf('state%d',ss_idx);
@@ -198,8 +271,10 @@ writetable(T2states_LifeTime_max,fullfile(output_path,'T2states_LifeTime_max.txt
 
 %between condition comparison
 state={};
-condition={};
+%condition={};
 pValue={};
+condition1={};
+condition2={};
 
 for s_idx=1:model.n_states
     for t_idx=1:cond_trial_nbr(1)
@@ -213,20 +288,26 @@ for s_idx=1:model.n_states
     end
     
     [p, tbl, stats]=anova1(anova_State_cond_mat);
-    [c,m,h,gnames]=multcompare(stats,'dimension',[1 2]);
+    
+    [c,m,h,gnames]=multcompare(stats,'dimension',[1 2],'Display','off');
 
+    stats=struct2table(stats,'AsArray',true);
+    writetable(stats,fullfile(output_path,sprintf('ANOVAstats_LifeTimeMax_state%d_3conditions.txt',s_idx)),'Delimiter','\t');
+    
     for c_idx=1:length(conditions)
         state{end+1}=sprintf('state%d',s_idx);
-        condition{end+1}=sprintf('condition%d',c_idx);
+        condition1{end+1}=sprintf('condition%d',c(c_idx,1));
+        condition2{end+1}=sprintf('condition%d',c(c_idx,2));
         pValue{end+1}=c(c_idx,6);
     end
 end
 
 state=state';
-condition=condition';
+condition1=condition1';
+condition2=condition2';
 pValue=pValue';
 
-T1state3cond_LifeTime_max=table(state,condition,pValue);
+T1state3cond_LifeTime_max=table(state,condition1,condition2,pValue);
 
 writetable(T1state3cond_LifeTime_max,fullfile(output_path,'T1state3cond_LifeTime_max.txt'),'Delimiter','\t');
 
@@ -278,6 +359,8 @@ for s_idx=1:model.n_states-1
             anova_State_mat(:,2)=eval(['LT_sum_S' num2str(ss_idx)]);
             
             [p, tbl, stats]=anova1(anova_State_mat);
+             stats=struct2table(stats,'AsArray',true);
+            writetable(stats,fullfile(output_path,sprintf('ANOVAstats_LifeTimeSum_state%d_state%d.txt',s_idx,ss_idx)),'Delimiter','\t');
             
             state1{end+1}=sprintf('state%d',s_idx);
             state2{end+1}=sprintf('state%d',ss_idx);
@@ -295,8 +378,10 @@ writetable(T2states_LifeTime_sum,fullfile(output_path,'T2states_LifeTime_sum.txt
 
 %between condition comparison
 state={};
-condition={};
+%condition={};
 pValue={};
+condition1={};
+condition2={};
 
 for s_idx=1:model.n_states
     for t_idx=1:cond_trial_nbr(1)
@@ -310,20 +395,26 @@ for s_idx=1:model.n_states
     end
     
     [p, tbl, stats]=anova1(anova_State_cond_mat);
+    
     [c,m,h,gnames]=multcompare(stats,'dimension',[1 2]);
 
+    stats=struct2table(stats,'AsArray',true);
+    writetable(stats,fullfile(output_path,sprintf('ANOVAstats_LifeTimeSum_state%d_3conditions.txt',s_idx)),'Delimiter','\t');
+    
     for c_idx=1:length(conditions)
         state{end+1}=sprintf('state%d',s_idx);
-        condition{end+1}=sprintf('condition%d',c_idx);
+        condition1{end+1}=sprintf('condition%d',c(c_idx,1));
+        condition2{end+1}=sprintf('condition%d',c(c_idx,2));
         pValue{end+1}=c(c_idx,6);
     end
 end
 
 state=state';
-condition=condition';
+condition1=condition1';
+condition2=condition2';
 pValue=pValue';
 
-T1state3cond_LifeTime_sum=table(state,condition,pValue);
+T1state3cond_LifeTime_sum=table(state,condition1,condition2,pValue);
 
 writetable(T1state3cond_LifeTime_sum,fullfile(output_path,'T1state3cond_LifeTime_sum.txt'),'Delimiter','\t');
 
@@ -374,6 +465,8 @@ for s_idx=1:model.n_states-1
             anova_State_mat(:,2)=eval(['ft_S' num2str(ss_idx)]);
             
             [p, tbl, stats]=anova1(anova_State_mat);
+             stats=struct2table(stats,'AsArray',true);
+            writetable(stats,fullfile(output_path,sprintf('ANOVAstats_fractionalOccupancy_state%d_state%d.txt',s_idx,ss_idx)),'Delimiter','\t');
             
             state1{end+1}=sprintf('state%d',s_idx);
             state2{end+1}=sprintf('state%d',ss_idx);
@@ -391,8 +484,10 @@ writetable(T2states_FractionalOccupancy,fullfile(output_path,'T2states_Fractiona
 
 %between condition comparison
 state={};
-condition={};
+%condition={};
 pValue={};
+condition1={};
+condition2={};
 
 for s_idx=1:model.n_states
     for t_idx=1:cond_trial_nbr(1)
@@ -406,19 +501,25 @@ for s_idx=1:model.n_states
     end
     
     [p, tbl, stats]=anova1(anova_State_cond_mat);
-    [c,m,h,gnames]=multcompare(stats,'dimension',[1 2]);
+    
+    [c,m,h,gnames]=multcompare(stats,'dimension',[1 2],'Display','off');
 
+    stats=struct2table(stats,'AsArray',true);
+    writetable(stats,fullfile(output_path,sprintf('ANOVAstats_FractionalOccupancy_state%d_3conditions.txt',s_idx)),'Delimiter','\t');
+    
     for c_idx=1:length(conditions)
         state{end+1}=sprintf('state%d',s_idx);
-        condition{end+1}=sprintf('condition%d',c_idx);
+        condition1{end+1}=sprintf('condition%d',c(c_idx,1));
+        condition2{end+1}=sprintf('condition%d',c(c_idx,2));
         pValue{end+1}=c(c_idx,6);
     end
 end
 state=state';
-condition=condition';
+condition1=condition1';
+condition2=condition2';
 pValue=pValue';
 
-T1state3cond_FractionalOccupancy=table(state,condition,pValue);
+T1state3cond_FractionalOccupancy=table(state,condition1,condition2,pValue);
 
 writetable(T1state3cond_FractionalOccupancy,fullfile(output_path,'T1state3cond_FractionalOccupancy.txt'),'Delimiter','\t');
 
@@ -475,7 +576,10 @@ for s_idx=1:model.n_states-1
             for t_idx=1:length(eval(['interval_S' num2str(ss_idx)]))
                 anova_State_mat(t_idx,2)=eval(['interval_S' num2str(ss_idx) '(t_idx)']);
             end
+            
             [p, tbl, stats]=anova1(anova_State_mat);
+             stats=struct2table(stats,'AsArray',true);
+            writetable(stats,fullfile(output_path,sprintf('ANOVAstats_sameStateInterval_state%d_state%d.txt',s_idx,ss_idx)),'Delimiter','\t');
             
             state1{end+1}=sprintf('state%d',s_idx);
             state2{end+1}=sprintf('state%d',ss_idx);
@@ -493,8 +597,10 @@ writetable(T2states_TimeIntervale,fullfile(output_path,'T2states_TimeIntervale.t
 
 %between condition comparison
 state={};
-condition={};
+%condition={};
 pValue={};
+condition1={};
+condition2={};
 
 for s_idx=1:model.n_states
     for t_idx=1:length(eval(['interval_S' num2str(s_idx) 'C1']))
@@ -508,19 +614,25 @@ for s_idx=1:model.n_states
     end
     
     [p, tbl, stats]=anova1(anova_State_cond_mat);
-    [c,m,h,gnames]=multcompare(stats,'dimension',[1 2]);
+    
+    [c,m,h,gnames]=multcompare(stats,'dimension',[1 2],'Display','off');
 
+     stats=struct2table(stats,'AsArray',true);
+    writetable(stats,fullfile(output_path,sprintf('ANOVAstats_SameStateInterval_state%d_3conditions.txt',s_idx)),'Delimiter','\t');
+    
     for c_idx=1:length(conditions)
         state{end+1}=sprintf('state%d',s_idx);
-        condition{end+1}=sprintf('condition%d',c_idx);
+        condition1{end+1}=sprintf('condition%d',c(c_idx,1));
+        condition2{end+1}=sprintf('condition%d',c(c_idx,2));
         pValue{end+1}=c(c_idx,6);
     end
 end
 state=state';
-condition=condition';
+condition1=condition1';
+condition2=condition2';
 pValue=pValue';
 
-T1state3cond_TimeIntervale=table(state,condition,pValue);
+T1state3cond_TimeIntervale=table(state,condition1,condition2,pValue);
 
 writetable(T1state3cond_TimeIntervale,fullfile(output_path,'T1state3cond_TimeIntervale.txt'),'Delimiter','\t');
 
@@ -571,6 +683,8 @@ for s_idx=1:model.n_states-1
             anova_State_mat(:,2)=eval(['blip_S' num2str(ss_idx)]);
             
             [p, tbl, stats]=anova1(anova_State_mat);
+             stats=struct2table(stats,'AsArray',true);
+            writetable(stats,fullfile(output_path,sprintf('ANOVAstats_blips_state%d_state%d.txt',s_idx,ss_idx)),'Delimiter','\t');
             
             state1{end+1}=sprintf('state%d',s_idx);
             state2{end+1}=sprintf('state%d',ss_idx);
@@ -588,8 +702,10 @@ writetable(T2states_Blips,fullfile(output_path,'T2states_Blips.txt'),'Delimiter'
 
 %between condition comparison
 state={};
-condition={};
+%condition={};
 pValue={};
+condition1={};
+condition2={};
 
 for s_idx=1:model.n_states
     for t_idx=1:cond_trial_nbr(1)
@@ -603,19 +719,25 @@ for s_idx=1:model.n_states
     end
     
     [p, tbl, stats]=anova1(anova_State_cond_mat);
-    [c,m,h,gnames]=multcompare(stats,'dimension',[1 2]);
+    
+    [c,m,h,gnames]=multcompare(stats,'dimension',[1 2],'Display','off');
 
+     stats=struct2table(stats,'AsArray',true);
+    writetable(stats,fullfile(output_path,sprintf('ANOVAstats_blip_state%d_3conditions.txt',s_idx)),'Delimiter','\t');
+    
     for c_idx=1:length(conditions)
         state{end+1}=sprintf('state%d',s_idx);
-        condition{end+1}=sprintf('condition%d',c_idx);
+        condition1{end+1}=sprintf('condition%d',c(c_idx,1));
+        condition2{end+1}=sprintf('condition%d',c(c_idx,2));
         pValue{end+1}=c(c_idx,6);
     end
 end
 state=state';
-condition=condition';
+condition1=condition1';
+condition2=condition2';
 pValue=pValue';
 
-T1state3cond_Blips=table(state,condition,pValue);
+T1state3cond_Blips=table(state,condition1,condition2,pValue);
 
 writetable(T1state3cond_Blips,fullfile(output_path,'T1state3cond_Blips.txt'),'Delimiter','\t');
 
