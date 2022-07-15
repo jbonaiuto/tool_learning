b516481c5bc6e3bccdf3865a1f92fa0d7c6b997e
James Bonaiuto <bonaiuto@n18h202.isc.cnrs.fr>
2022-07-15 13:47:50 +0200

    fixed merge

--- src/matlab/hmm/mHMMBayes/tv_cov/runHMM_tc_cov_new_grasp.m
+++ src/matlab/hmm/mHMMBayes/tv_cov/runHMM_tc_cov_new_grasp.m
@@ -1,11 +1,7 @@
-%runHMM_tc_cov_new_grasp(exp_info, subject, array)
+function runHMM_tc_cov_new_grasp(exp_info, subject, array)
 
 addpath('../../..');
 addpath('../../../spike_data_processing');
-
-exp_info=init_exp_info();
-subject='betta';
-array='F1';
 
 % Conditions to run on
 conditions={'motor_grasp_center','motor_grasp_right','motor_grasp_left'};
@@ -13,15 +9,6 @@ conditions={'motor_grasp_center','motor_grasp_right','motor_grasp_left'};
 if strcmp(subject,'betta')
     dates={'26.02.19','27.02.19','28.02.19','01.03.19','04.03.19',...
        '05.03.19','07.03.19','08.03.19','11.03.19','12.03.19'};
-%    dates={'26.02.19','27.02.19','28.02.19','01.03.19','04.03.19','05.03.19','07.03.19','08.03.19','11.03.19','12.03.19','13.03.19','14.03.19','15.03.19','18.03.19',...
-%       '19.03.19','20.03.19','21.03.19','25.03.19','26.03.19','27.03.19','28.03.19','29.03.19','01.04.19','02.04.19','03.04.19','05.04.19','08.04.19','09.04.19',...
-%       '10.04.19','12.04.19','15.04.19','16.04.19','17.04.19','18.04.19','19.04.19','23.04.19','24.04.19','26.04.19','03.05.19','07.05.19','09.05.19','14.05.19',...
-%       '15.05.19','16.05.19','17.05.19','20.05.19','21.05.19','22.05.19','23.05.19','13.06.19','14.06.19','19.06.19','25.06.19','26.06.19','28.06.19','01.07.19',...
-%       '02.07.19','03.07.19','04.07.19','05.07.19','08.07.19','11.07.19','12.07.19','15.07.19','17.07.19','18.07.19','19.07.19','22.07.19','23.07.19','24.07.19',...
-%       '26.07.19','29.07.19','31.07.19','01.08.19','02.08.19','05.08.19','06.08.19','07.08.19','09.08.19','27.08.19','28.08.19','29.08.19','04.09.19','05.09.19',...
-%       '06.09.19','09.09.19','10.09.19','12.09.19','18.09.19','25.09.19','27.09.19','04.10.19','07.10.19','08.10.19','10.10.19','11.10.19','14.10.19','16.10.19',...
-%       '17.10.19','18.10.19','22.10.19','28.10.19','29.10.19','04.11.19','06.11.19','14.11.19','20.11.19','21.11.19','25.11.19'};
-   
    
     %good channels sample
     % for F1
@@ -37,7 +24,7 @@ elseif strcmp(subject,'samovar')
     %good channels sample
     % for F1
     if strcmp(array, 'F1')
-        electrodes=[4 5 6 7 9 10 11 13 14 15 16 17 18 19 20 21 22 23 25 26 27 28 29 30 31];
+        electrodes=[4 5 6 13 14 16 17 18 19 20 21 22 23 25 26 27 28];
     end
 end
 
@@ -54,17 +41,17 @@ if exist(output_path,'dir')~=7
     mkdir(output_path);
 end
 
-% if exist(fullfile(output_path,'data.mat'),'file')~=2
-%     %Otherwise export to CSV and save
-%     data=export_data_to_csv(exp_info, subject, array, conditions, dates,...
-%         dt, output_path, electrodes);
-%     save(fullfile(output_path,'data.mat'),'data','-v7.3');
-%     clear data;
-% end
-% 
-% % Fit the model
-% system(sprintf('/home/bonaiuto/miniconda3/envs/hmm/bin/Rscript ../../../../R/hmm/fit_plnorm_tv_covar_new.R "%s"',...
-%      strrep(output_path,'\','/')));
+if exist(fullfile(output_path,'data.mat'),'file')~=2
+    %Otherwise export to CSV and save
+    data=export_data_to_csv(exp_info, subject, array, conditions, dates,...
+        dt, output_path, electrodes);
+    save(fullfile(output_path,'data.mat'),'data','-v7.3');
+    clear data;
+end
+ 
+% Fit the model
+system(sprintf('/home/bonaiuto/miniconda3/envs/hmm/bin/Rscript ../../../../R/hmm/fit_plnorm_tv_covar_new.R "%s"',...
+     strrep(output_path,'\','/')));
 
 % Load best model (lowest AIC)
 model=get_best_model(output_path, 'type', 'plnorm');
@@ -78,7 +65,7 @@ plot_model_params(subject, array, model, conditions,output_path);
 
 run_state_trial_stats(model, data, dates, conditions);
 
-run_perm_test_events(subject, array, data, model, conditions, dates, output_path);
+run_perm_test_events(data, model, dates)
 
 system(sprintf('"C:/Program Files/R/R-4.1.3/bin/Rscript" ../../../R/hmm/shuffle_electrode_plnorm_tv_covar_new.R "%s" %d 1',...
     strrep(output_path,'\','/'), model.n_states));
